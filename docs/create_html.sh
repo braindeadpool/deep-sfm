addd02cd4c6dd5d4293e945209d59ec8eba7102f
Suraj M S <braindeadpool@gmail.com>
2018-03-22 02:52:55 +0000

    update create html script

--- docs/create_html.sh
+++ docs/create_html.sh
@@ -1,2 +1,2 @@
-pandoc --filter pandoc-citeproc --bibliography=references.bib --variable classoption=twocolumn --variable papersize=a4pape -s report.md -t html -o index.html
+pandoc --filter pandoc-citeproc --bibliography=references.bib --variable classoption=twocolumn --variable papersize=a4pape -s report.md -t html -o ../index.html
 
